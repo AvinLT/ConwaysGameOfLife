a17c53b97991813570d4543bd4aafd85d69842dd
Avin Lanson <avinlanson@gmail.com>
2021-06-24 15:46:26 +0300

    clicking now activates squares, made grid bigger

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import pygame
 import sys
 from pygame.locals import *
 
-# import random
-
 # GAME RULES
-# 1)Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+# 1)Any live cell with fewer than two live neighbours dies, as if by underpopulation. UNDER_THRESH = 2
 # 2)Any live cell with two or three live neighbours lives on to the next generation.
-# 3)Any live cell with more than three live neighbours dies, as if by overpopulation.
-# 4)Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+# 3)Any live cell with more than three live neighbours dies, as if by overpopulation. OVER_THRESH = 3
+# 4)Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction. REPR_THRESH = 3
+
+# GAME VARIABLES
+UNDER_THRESH = 2
+OVER_THRESH = 3
+REPR_THRESH = 3
+
+CONST_SIZE = 10  # square size
+CONST_GRID_OFFSET = [30, 25]  # move entire grid by
+CONST_EDGE_IGNORE = 1  # make outer squares on grid unusable, to enclose chain reaction
+
+# RGB color codes
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (30, 30, 40)
+BLUE = (160, 240, 255)
+GREEN = (127, 255, 212)
 
 clock = pygame.time.Clock()  # initialize clock
 pygame.init()  # initialize pygame
@@ -16,17 +30,6 @@ pygame.init()  # initialize pygame
 WINDOW_SIZE = (770, 600)  # window size
 display = pygame.Surface((770, 600))  # what we display images on.
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # initialize window
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREY = (30, 30, 40)
-CONST_SIZE = 18
-CONST_GRID_OFFSET = [65, 25]
-
-# GAME VAR
-UNDER_THRESH = 2
-OVER_THRESH = 3
-REPR_THRESH = 3
 
 
 # load map from GameMap.txt
@@ -42,14 +45,27 @@ def load_text_file(path):
 
 
 def make_grid(text_map):
-    y = 0
+    len_y = len(text_map)
+    len_x = len(text_map[0])
+    edge = False
     map = []
-    for row in text_map:
+    alive = False
+
+    for y in range(len_y):
         temp = []
         x = 0
-        for _ in row:
-            temp.append(Square(x * CONST_SIZE + CONST_GRID_OFFSET[0], y * CONST_SIZE + CONST_GRID_OFFSET[1]))
+        for _ in range(len_x):
+            if y < CONST_EDGE_IGNORE or y > len_y - CONST_EDGE_IGNORE - 1:
+                edge = True
+            if x < CONST_EDGE_IGNORE or x > len_x - CONST_EDGE_IGNORE - 1:
+                edge = True
+            if text_map[y][x] == '1':
+                alive = True
+            temp.append(
+                Square(x * CONST_SIZE + CONST_GRID_OFFSET[0], y * CONST_SIZE + CONST_GRID_OFFSET[1], edge, alive))
             x += 1
+            edge = False
+            alive = False
         map.append(temp)
         y += 1
     return map
@@ -58,54 +74,59 @@ def make_grid(text_map):
 def draw_grid(grid_squares):
     for row in grid_squares:
         for square in row:
-            if square.touch_cursor:
+            if square.alive:
                 pygame.draw.rect(display, WHITE, square.rect)
             else:
                 pygame.draw.rect(display, BLACK, square.rect)
+            if square.touch_cursor:
+                pygame.draw.rect(display, WHITE, square.rect)
             pygame.draw.rect(display, GREY, square.rect, 1)
 
 
-def cursor_on_square(grid_squares, cursor_loc):
+def cursor_on_square(grid_squares, cursor_loc, click):
     for row in grid_squares:
         for square in row:
             if pygame.Rect.collidepoint(square.rect, cursor_loc):
                 square.touch_cursor = True
+                if click:
+                    square.alive = True
             else:
                 square.touch_cursor = False
 
 
 class Square:
-    def __init__(self, x, y):
+    def __init__(self, x, y, edge_square, alive):
         self.x = x
         self.y = y
         self.width = 12
         self.loc = [int((x - CONST_GRID_OFFSET[0]) / CONST_SIZE), int((y - CONST_GRID_OFFSET[1]) / CONST_SIZE)]
-        self.alive = False
+        self.alive = alive
         self.rect = pygame.Rect(x, y, CONST_SIZE, CONST_SIZE)
         self.touch_cursor = False
         self.neighbors = 0
+        self.grid_edge = edge_square
 
     def tot_neighbors(self, grid_squares):
         tot = 0
-        for x in (-1, -1, 0, 1, 1, 1, 0, -1):
-            for y in (0, 1, 1, 1, 0, -1, -1, -1):
+        for y, x in ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)):
+            if not grid_squares[self.loc[1]][self.loc[0]].grid_edge:
                 if grid_squares[self.loc[1] + y][self.loc[0] + x].alive:
                     tot += 1
         self.neighbors = tot
 
-    def die_method(self):
-        if self.neighbors < UNDER_THRESH:
-            self.alive = False
-        elif self.neighbors > OVER_THRESH:
-            self.alive = False
-
-    def alive_method(self):
-        if self.neighbors == REPR_THRESH:
-            self.alive = True
+    def die_alive_method(self):
+        if not self.grid_edge:
+            if self.neighbors == REPR_THRESH:
+                self.alive = True
+            if self.neighbors < UNDER_THRESH:
+                self.alive = False
+            elif self.neighbors > OVER_THRESH:
+                self.alive = False
 
 
 text_output = load_text_file("GameMap")
 grid = make_grid(text_output)
+click_state = False
 
 while True:  # Main game loop
 
@@ -118,9 +139,21 @@ while True:  # Main game loop
         if event.type == QUIT:  # checks if window is closed
             pygame.quit()  # stops pygame
             sys.exit()  # stops script
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:  # left click
+                click_state = True
+        if event.type == MOUSEBUTTONUP:
+            if event.button == 1:  # left click
+                click_state = False
 
-    # start here
-    cursor_on_square(grid, loc)
+    for row_of_squares in grid:
+        for single_square in row_of_squares:
+            single_square.tot_neighbors(grid)
+    for row_of_squares in grid:
+        for single_square in row_of_squares:
+            single_square.die_alive_method()
+
+    cursor_on_square(grid, loc, click_state)
     draw_grid(grid)
     # end here
 
@@ -128,4 +161,4 @@ while True:  # Main game loop
     mainDisplay = pygame.transform.scale(display, WINDOW_SIZE)
     screen.blit(mainDisplay, (0, 0))
 
-    clock.tick(60)  # set frame rate
+    clock.tick(20)  # set frame rate
